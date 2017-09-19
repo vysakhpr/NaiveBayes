07921e8bb01465f5b04d12c7e2d872aeabab54d1
Vysakh P R <my_mail_is_buzy@yahoo.com.au>
2017-09-19 12:58:27 +0530

    Minor changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ start=time.time()
 f=open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_train.txt",'r')
 for line in f:
 	parts=line.split("\t")
-	labels=parts[0].split(',')
+	labels=parts[0].replace(" ","").split(',')
 	document=parts[1].split('"',1)[1].rsplit('"',1)[0].replace("\\u","").translate(None,string.punctuation).translate(None,string.digits)
 	words=filter(None,document.split(" "))
 	words=[word for  word in words if word.lower() not in months]
@@ -42,6 +42,8 @@ for line in f:
 qx=1.0/len(word_label_dict)
 qy=1.0/len(label_dict)
 m=1
+# for label in label_dict:
+# 	print label,len(label)
 for word in word_label_dict:
 	for label in label_dict:
 		if label not in word_label_dict[word]:
@@ -50,11 +52,13 @@ for word in word_label_dict:
 end = time.time()
 print "Time Taken for Training: ",(end - start)
 
-f=open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_test.txt",'r')
+start=time.time()
+
+f=open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_train.txt",'r')
 for line in f:
 	label_probability_dict={}
 	parts=line.split("\t")
-	labels=parts[0].split(',')
+	labels=parts[0].replace(" ","").split(',')
 	document=parts[1].split('"',1)[1].rsplit('"',1)[0].replace("\\u","").translate(None,string.punctuation).translate(None,string.digits)
 	words=filter(None,document.split(" "))
 	words=[word for  word in words if word.lower() not in months]
@@ -66,18 +70,76 @@ for line in f:
 		label_probability_dict[label]=sum+math.log((label_dict[label]+m*qy)/(label_count+m))
 	max_label=max(label_probability_dict.iteritems(),key=operator.itemgetter(1))[0]
 	predictions.append(max_label)
-	truth.append(parts[0])
+	truth.append(parts[0].replace(" ",""))
 
 end = time.time()
-print "Time Taken for Training and Testing: ",(end - start)
-
-
+print "Time Taken for Testing on Train Data: ",(end - start)
 sum = 0
 for i in range(0,len(predictions)):
 	x=truth[i].split(',')
 	if predictions[i] in x:
 		sum=sum+1
-print sum*100.0/len(predictions)
+print "Accuracy :", sum*100.0/len(predictions), "%"
+predictions=[]
+truth=[]
+
+start=time.time()
+f=open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_test.txt",'r')
+for line in f:
+	label_probability_dict={}
+	parts=line.split("\t")
+	labels=parts[0].replace(" ","").split(',')
+	document=parts[1].split('"',1)[1].rsplit('"',1)[0].replace("\\u","").translate(None,string.punctuation).translate(None,string.digits)
+	words=filter(None,document.split(" "))
+	words=[word for  word in words if word.lower() not in months]
+	for label in label_dict:
+		sum=0
+		for word in words:
+			t=(word_label_dict[word][label]+m*qx)/(label_anyword_dict[label]+m)
+			sum=sum+math.log(t)
+		label_probability_dict[label]=sum+math.log((label_dict[label]+m*qy)/(label_count+m))
+	max_label=max(label_probability_dict.iteritems(),key=operator.itemgetter(1))[0]
+	predictions.append(max_label)
+	truth.append(parts[0].replace(" ",""))
+
+end = time.time()
+print "Time Taken for Testing on Test Data: ",(end - start)
+sum = 0
+for i in range(0,len(predictions)):
+	x=truth[i].split(',')
+	if predictions[i] in x:
+		sum=sum+1
+print "Accuracy :", sum*100.0/len(predictions), "%"
+predictions=[]
+truth=[]
+
+start=time.time()
+f=open("/scratch/ds222-2017/assignment-1/DBPedia.full/full_devel.txt",'r')
+for line in f:
+	label_probability_dict={}
+	parts=line.split("\t")
+	labels=parts[0].replace(" ","").split(',')
+	document=parts[1].split('"',1)[1].rsplit('"',1)[0].replace("\\u","").translate(None,string.punctuation).translate(None,string.digits)
+	words=filter(None,document.split(" "))
+	words=[word for  word in words if word.lower() not in months]
+	for label in label_dict:
+		sum=0
+		for word in words:
+			t=(word_label_dict[word][label]+m*qx)/(label_anyword_dict[label]+m)
+			sum=sum+math.log(t)
+		label_probability_dict[label]=sum+math.log((label_dict[label]+m*qy)/(label_count+m))
+	max_label=max(label_probability_dict.iteritems(),key=operator.itemgetter(1))[0]
+	predictions.append(max_label)
+	truth.append(parts[0].replace(" ",""))
+
+end = time.time()
+print "Time Taken for Testing on Development data: ",(end - start)
+sum = 0
+for i in range(0,len(predictions)):
+	x=truth[i].split(',')
+	if predictions[i] in x:
+		sum=sum+1
+print "Accuracy :", sum*100.0/len(predictions), "%"
 		
 
 
